7c1c19cb89b61b47efc837006759219696974ef1
KNU-K <knukang334@gmail.com>
2024-09-20 19:19:04 +0900

    feat : init workbook domain

---
+++ src/main/java/io/github/muktae/be_codebase/domain/workbook/domain/WorkBook.java
@@ -0,0 +1,29 @@
+package io.github.muktae.be_codebase.domain.workbook.domain;
+
+
+import io.github.muktae.be_codebase.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bookmark")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    private String problemSet;
+
+}
