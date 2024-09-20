8b224a04cee5abb863b20c8c52028a3626a78c03
KNU-K <knukang334@gmail.com>
2024-09-20 19:19:43 +0900

    feat : add bookmark domain

---
+++ src/main/java/io/github/muktae/be_codebase/domain/bookmark/domain/Bookmark.java
@@ -0,0 +1,31 @@
+package io.github.muktae.be_codebase.domain.bookmark.domain;
+
+import io.github.muktae.be_codebase.common.entity.BaseEntity;
+import io.github.muktae.be_codebase.domain.record.domain.Record;
+import io.github.muktae.be_codebase.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bookmark")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bookmark extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "records_id")
+    private Record record;
+
+}
